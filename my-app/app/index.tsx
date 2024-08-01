9e3c96d060500a895893a02eba55dbe256ba32ae
themxahmed00 <ma2k510@gmail.com>
2024-08-02 05:04:35 +0530

    Update greeting text in index.tsx

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -13,7 +13,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>hello Team</Text>
+      <Text>hello Team mate</Text>
       <TouchableOpacity onPress={() => {
         console.log('Button pressed!');
         alert('Button pressed!');
